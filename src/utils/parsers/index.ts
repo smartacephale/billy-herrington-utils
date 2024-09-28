56f6fa5adfc5b1eed9e33ec08bfd2e9a4270c5c9
smartacephale <atm.mormon@protonmail.com>
2024-09-28 21:12:22 +0000

    parseDataParams

--- src/utils/parsers/index.ts
+++ src/utils/parsers/index.ts
@@ -6,17 +6,21 @@ export function timeToSeconds(t: string): number {
 }
 
 export function parseIntegerOr(n: string | number, or: number): number {
-  return Number.isInteger(parseInt(n as string)) ? parseInt(n as string) : or;
+  return (num => Number.isNaN(num) ? or : num)(parseInt(n as string));
 }
 
 // "data:02;body+head:async;void:;zero:;"
-export function parseDataParams(str: string) {
-  const params = str.split(';').flatMap(s => {
-    const parsed = s.match(/([\+\w+]+):(\w+)?/);
-    const value = parsed?.[2];
-    if (value) return parsed[1].split('+').map(p => ({ [p]: value }));
-  }).filter(_ => _);
-  return Object.assign({}, ...params);
+export function parseDataParams(str: string): Record<string, string> {
+  return str.split(';').reduce((acc, s) => {
+    const parsed = s.match(/([\+\w]+):(\w+)?/);
+    if (parsed) {
+      const [, key, value] = parsed;
+      if (value) {
+        key.split('+').forEach(p => { acc[p] = value; });
+      }
+    }
+    return acc;
+  }, {} as Record<string, string>);
 }
 
 export function parseCSSUrl(s: string) {
