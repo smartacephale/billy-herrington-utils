b9754bb20747f45d6b07a88b4d783971aa34f9fc
smartacephale <atm.mormon@protonmail.com>
2024-09-28 17:48:36 +0000

    spool refact

--- src/utils/async/index.ts
+++ src/utils/async/index.ts
@@ -11,7 +11,7 @@ export function wait(milliseconds: number) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
-interface SyncPoolObject {
+interface AsyncPoolTask {
   v: () => Promise<void>;
   p: number;
 }
@@ -21,51 +21,66 @@ export class AsyncPool {
   private finished: Promise<boolean>;
   private _resolve?: (value: boolean | PromiseLike<boolean>) => void;
 
-  constructor(
-    private max = 1,
-    private pool: Array<SyncPoolObject> = [],
-  ) {
+  public static async doNAsyncAtOnce(max = 1, pool: Array<AsyncPoolTask | (() => Promise<void>)>) {
+    const spool = new AsyncPool(max);
+    pool.forEach(f => spool.push(f));
+    return spool.run();
+  }
+
+  constructor(private max = 1, private pool: Array<AsyncPoolTask> = []) {
     this.finished = new Promise((resolve) => {
       this._resolve = resolve;
     });
   }
 
-  getHighPriorityFirst(p = 0): (() => Promise<void>) | undefined {
+  private getHighPriorityFirst(p = 0): (() => Promise<void>) | undefined {
     if (p > 3 || this.pool.length === 0) return undefined;
     const i = this.pool.findIndex((e) => e.p === p);
     if (i >= 0) {
       const res = this.pool[i].v;
-      this.pool = this.pool.slice(0, i).concat(this.pool.slice(i + 1));
+      this.pool.splice(i, 1);
       return res;
     }
     return this.getHighPriorityFirst(p + 1);
   }
 
-  async runTask() {
-    this.cur++;
-    const f = this.getHighPriorityFirst();
-    await f?.();
-    this.cur--;
-    this.runTasks();
-  }
-
-  runTasks() {
-    if (!this.pool.length) {
-      this._resolve?.(true);
+  private async runTask() {
+    const taskFunc = this.getHighPriorityFirst();
+    if (!taskFunc) {
+      this.checkCompletion();
       return;
     }
-    if (this.cur < this.max) {
-      this.runTask();
+
+    this.cur++;
+    try {
+      await taskFunc();
+    } catch (error) {
+      console.error("Task execution failed:", error);
+    } finally {
+      this.cur--;
       this.runTasks();
     }
   }
 
-  async run() {
+  private checkCompletion() {
+    if (this.pool.length === 0 && this.cur === 0) {
+      this._resolve?.(true);
+    }
+  }
+
+  private runTasks() {
+    while (this.cur < this.max) {
+      this.runTask();
+    }
+  }
+
+  public async run() {
     this.runTasks();
     return this.finished;
   }
 
-  push(x: SyncPoolObject | (() => Promise<void>)) {
+  public push(x: AsyncPoolTask | (() => Promise<void>)) {
     this.pool.push('p' in x ? x : { v: x, p: 0 });
   }
 }
+
